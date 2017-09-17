ea7767f1c379171caceb45f736c699e959d5aa24
Adelbert Chang <adelbertc@gmail.com>
2017-09-17 13:54:30 +0100

    Update nixconfig

--- nixpkgs/.config/nixpkgs/config.nix
+++ nixpkgs/.config/nixpkgs/config.nix
@@ -1,9 +1,11 @@
 {
   packageOverrides = pkgs: rec {
-    myDevStuff     = with pkgs; { inherit git neovim nix-repl /* ripgrep */ stow; };
+    myDevStuff      = with pkgs; { inherit git neovim nix-repl ripgrep stow; };
 
-    myHaskellStuff = with pkgs; { inherit cabal-install cabal2nix /* stack */ ; };
+    myHaskellStuff  = with pkgs; { inherit cabal-install cabal2nix stack; };
 
-    myScalaStuff   = with pkgs; { inherit sbt; };
+    myTexStuff      = with pkgs; { inherit (texlive.combined) scheme-full; };
+
+    myScalaStuff    = with pkgs; { inherit sbt; };
   };
 }
